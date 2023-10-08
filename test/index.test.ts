cbc519841021d5c08c0732658e5b4c88611dffc9
lafkpages <soy.lafk+gh@gmail.com>
2023-10-08 23:05:03 +0200

    Separate Asar.header tests, refactor a bit

--- test/index.test.ts
+++ test/index.test.ts
@@ -4,7 +4,7 @@ import { Asar, Entry, Header } from "../src";
 import { readFile, writeFile } from "fs/promises";
 
 const asarData = await readFile("test/ignore/app.asar");
-let asar: Asar | null = null;
+let asar: Asar;
 
 // This test assumes that the ASAR file given belongs
 // to an Electron app, and has a package.json file.
@@ -13,6 +13,9 @@ test("new Asar", () => {
   asar = new Asar(asarData);
 
   expect(asar).toBeInstanceOf(Asar);
+});
+
+test("Asar.header", () => {
   expect(asar.headerSize).toBeNumber();
   expect(asar.rawHeader).toBeString();
 
@@ -21,36 +24,28 @@ test("new Asar", () => {
   expect(asar.header).toBeInstanceOf(Header);
 });
 
-let packageJsonEntry: Entry | null = null;
+let packageJsonEntry: Entry;
 
 test("Header.getFromPath", () => {
-  packageJsonEntry = asar?.header.getFromPath("package.json") ?? null;
+  packageJsonEntry = asar.header.getFromPath("package.json");
 
   expect(packageJsonEntry).toBeInstanceOf(Entry);
 });
 
 test("Entry.isFile", () => {
-  if (packageJsonEntry == null) {
-    return;
-  }
-
   expect(Entry.isFile(packageJsonEntry)).toBeTrue();
 });
 
 test("Entry.isDirectory", () => {
-  if (packageJsonEntry == null) {
-    return;
-  }
-
   expect(Entry.isDirectory(packageJsonEntry)).toBeFalse();
 });
 
 test("Asar.readFile", async () => {
-  const packageJson = asar?.readFile("package.json")?.toString() ?? null;
+  const packageJson = asar.readFile("package.json").toString();
 
   expect(packageJson).toBeString();
 
   await writeFile("test/ignore/asar-package.json", packageJson!);
 
-  expect(packageJson?.[0]).toBe("{");
+  expect(packageJson[0]).toBe("{");
 });
