4a0c1680e0f344b613f5b6d7da35c93d820536ef
lafkpages <soy.lafk+gh@gmail.com>
2023-10-08 22:57:52 +0200

    Test script rename package.json file

--- test/index.test.ts
+++ test/index.test.ts
@@ -50,7 +50,7 @@ test("readFile", async () => {
 
   expect(packageJson).toBeString();
 
-  await writeFile("test/ignore/package.json", packageJson!);
+  await writeFile("test/ignore/asar-package.json", packageJson!);
 
   expect(packageJson?.[0]).toBe("{");
 });
