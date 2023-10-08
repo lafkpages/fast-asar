ec47a9721a8d22dd7322a79062e4ed713801a34b
lafkpages <soy.lafk+gh@gmail.com>
2023-10-08 10:38:13 +0200

    isDirectory tests

--- test/index.test.ts
+++ test/index.test.ts
@@ -12,4 +12,11 @@ test("Init ASAR", () => {
   expect(asar.headerSize).toBeNumber();
   expect(asar.rawHeader).toBeString();
   expect(asar.header).toBeInstanceOf(Header);
+
+  // This test assumes that the ASAR file given belongs
+  // to an Electron app, and has a package.json file.
+
+  const packageJsonIsDirectory = asar.isDirectory("package.json");
+
+  expect(packageJsonIsDirectory).toBeFalse();
 });
