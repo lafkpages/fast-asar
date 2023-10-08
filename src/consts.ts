0a11d98845a57eac789e90a2a0564c6be920ba7e
lafkpages <soy.lafk+gh@gmail.com>
2023-10-08 09:58:35 +0200

    Export consts as consts

--- src/consts.ts
+++ src/consts.ts
@@ -1,4 +1,7 @@
-export const headerMetadata = {
+export const headerSizeMetadata = {
   offset: 0,
   size: 8,
-};
+} as const;
+export const headerMetadata = {
+  offset: headerSizeMetadata.offset + headerSizeMetadata.size,
+} as const;
