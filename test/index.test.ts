6afd4c5e72ec22b3f0cb5cbe96d3e0c2a77e6830
lafkpages <soy.lafk+gh@gmail.com>
2023-10-08 10:44:23 +0200

    isFile tests

--- test/index.test.ts
+++ test/index.test.ts
@@ -16,6 +16,8 @@ test("Init ASAR", () => {
   // This test assumes that the ASAR file given belongs
   // to an Electron app, and has a package.json file.
 
+  expect(asar.isFile("package.json")).toBeTrue();
+  expect(asar.isFile("./package.json")).toBeTrue();
   expect(asar.isDirectory("package.json")).toBeFalse();
   expect(asar.isDirectory("./package.json")).toBeFalse();
   expect(asar.isDirectory("foo/bar/../../package.json")).toBeFalse();
