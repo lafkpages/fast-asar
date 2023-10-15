f1922a4115173ea3ebe73389c3b54931331e2223
lafkpages <soy.lafk+gh@gmail.com>
2023-10-15 22:07:03 +0000

    Logs module error function reset colors in message

--- src/log/index.ts
+++ src/log/index.ts
@@ -1,7 +1,7 @@
 import { colors } from "./colors";
 
 export function error(message: string) {
-  console.error(colors.red("error") + colors.gray(":"), message);
+  console.error(colors.red("error") + colors.gray(":"), colors.reset(message));
 }
 
 export const ok = colors.ok;
