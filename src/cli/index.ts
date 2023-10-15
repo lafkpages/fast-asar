84958961628f7b1e8ee6a41031cc0617dfabdd52
lafkpages <soy.lafk+gh@gmail.com>
2023-10-15 22:02:05 +0000

    Use error function from log module in CLI

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -1,14 +1,11 @@
 #!/usr/bin/env node
 
 import { help } from "./help";
-import { colors } from "../log";
+import { error } from "../log";
 
 const command = process.argv[2];
 
 if (!command) {
-  console.error(
-    colors.red("error") + colors.gray(":"),
-    "No command specified\n"
-  );
+  error("No command specified\n");
   help(2);
 }
