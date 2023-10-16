82824ef220fc80b7c7f2d8c64c30719b5863a3a8
lafkpages <soy.lafk+gh@gmail.com>
2023-10-16 10:20:00 +0200

    CLI unknown command error

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -1,11 +1,18 @@
 #!/usr/bin/env node
 
 import { help } from "./help";
-import { error } from "../log";
+import { colors, error } from "../log";
 
 const command = process.argv[2];
 
 if (!command) {
   error("No command specified\n");
+  help(2);
+}
+
+const commands = ["extract", "pack", "list", "help"];
+
+if (!commands.includes(command)) {
+  error(`Unknown command ${colors.bold(`"${command}"`)}\n`);
   help(2);
 }
