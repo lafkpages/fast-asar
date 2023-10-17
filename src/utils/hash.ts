a54176a53db31c22171fa6289b4eaf196fa74bd5
lafkpages <soy.lafk+gh@gmail.com>
2023-10-17 19:04:59 +0200

    sha256 hash util JSDoc

--- src/utils/hash.ts
+++ src/utils/hash.ts
@@ -1,5 +1,14 @@
 import { createHash } from "crypto";
 
+/**
+ * Hashes the given data using SHA256.
+ *
+ * If running in Bun, this will use `Bun.CryptoHasher`.
+ * Otherwise, it will use Node's `crypto` module.
+ *
+ * @param data The data to hash
+ * @returns The hash of the data
+ */
 export function sha256(data: string | Uint8Array): string {
   // If running in Bun, use Bun.CryptoHasher
   // instead of Node's crypto module because
