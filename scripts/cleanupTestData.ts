396c2d1dfefa0157216e897d62d163e30449ced3
lafkpages <soy.lafk+gh@gmail.com>
2023-10-17 11:38:23 +0200

    Remove old Bash comments in cleanup test data script

--- scripts/cleanupTestData.ts
+++ scripts/cleanupTestData.ts
@@ -1,9 +1,6 @@
 import { rm, readdir } from "fs/promises";
 import { join as joinPaths } from "path";
 
-// rm -f test/ignore/*.{js,json,txt}
-// rm -f test/ignore/app-*.asar
-
 const ignoreDir = "test/ignore";
 
 for (const file of await readdir(ignoreDir)) {
