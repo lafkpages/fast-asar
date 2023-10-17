bae16fe091c0fe62868a73da30e8a3f55c9c19e5
lafkpages <soy.lafk+gh@gmail.com>
2023-10-17 11:39:31 +0200

    Cleanup script also remove directories

--- scripts/cleanupTestData.ts
+++ scripts/cleanupTestData.ts
@@ -4,7 +4,10 @@ import { join as joinPaths } from "path";
 const ignoreDir = "test/ignore";
 
 for (const file of await readdir(ignoreDir)) {
-  if (/\.(js|json|txt)$|^app-.+\.asar/.test(file)) {
-    await rm(joinPaths(ignoreDir, file));
+  if (/\.(js|json|txt|extracted)$|^app-.+\.asar/.test(file)) {
+    await rm(joinPaths(ignoreDir, file), {
+      force: true,
+      recursive: true,
+    });
   }
 }
