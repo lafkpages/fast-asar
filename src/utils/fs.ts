a6e3a606638c331ac9fb68efb8a3df12fd448371
lafkpages <soy.lafk+gh@gmail.com>
2023-10-17 19:02:25 +0200

    walk util JSDoc

--- src/utils/fs.ts
+++ src/utils/fs.ts
@@ -1,6 +1,11 @@
 import { readdir } from "fs/promises";
 import { join as joinPaths } from "path";
 
+/**
+ * Walks through a directory and recursively yields all files and directories.
+ * @param dir The directory to walk through.
+ * @param includeDirectories Whether to include directories in the result.
+ */
 export async function* walk(
   dir: string,
   includeDirectories = false
