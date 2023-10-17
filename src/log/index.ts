f51104126b4a26dc716ebb0bfd430efed5f2cc94
lafkpages <soy.lafk+gh@gmail.com>
2023-10-17 19:27:08 +0200

    error log func JSDoc

--- src/log/index.ts
+++ src/log/index.ts
@@ -1,5 +1,10 @@
 import { colors } from "./colors";
 
+/**
+ * Logs an error message to the console.
+ * This will be prefixed with "error:" in red.
+ * @param message The message to log.
+ */
 export function error(message: string) {
   console.error(colors.red("error") + colors.gray(":"), colors.reset(message));
 }
