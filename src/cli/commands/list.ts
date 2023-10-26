63801af945a897b57b5813d203985a87b0e63846
lafkpages <soy.lafk+gh@gmail.com>
2023-10-26 10:30:33 +0200

    CLI list command use Asar.fromFile

--- src/cli/commands/list.ts
+++ src/cli/commands/list.ts
@@ -1,15 +1,11 @@
 import { Asar } from "../..";
 
-import { readFile } from "fs/promises";
-
 import { join as joinPaths } from "path";
 
 export default async function list(...args: string[]) {
   const [archive] = args;
 
-  const asarBytes = await readFile(archive);
-
-  const asar = new Asar(asarBytes, {
+  const asar = await Asar.fromFile(archive, {
     noFileData: true,
   });
 
