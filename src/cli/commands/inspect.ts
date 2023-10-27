0d3ab3eff06272fc085a6b46b1084c1d770d4d97
lafkpages <soy.lafk+gh@gmail.com>
2023-10-27 10:25:33 +0200

    CLI inspect cmd always write header to .asar.json

--- src/cli/commands/inspect.ts
+++ src/cli/commands/inspect.ts
@@ -3,7 +3,8 @@ import { writeFile } from "fs/promises";
 import { Asar } from "../..";
 
 export default async function inspect(...args: string[]) {
-  const [archive, rawHeaderDest] = args;
+  const [archive] = args;
+  const dest = archive + ".json";
 
   const asar = await Asar.fromFile(archive, {
     storeInitialParseData: true,
@@ -17,11 +18,7 @@ export default async function inspect(...args: string[]) {
   if (asar.initialParseData?.header !== undefined) {
     const prettyHeader = JSON.stringify(asar.initialParseData.header, null, 2);
 
-    if (rawHeaderDest) {
-      await writeFile(rawHeaderDest, prettyHeader);
-      console.log("raw header written to", rawHeaderDest);
-    } else {
-      console.log("raw header:", prettyHeader);
-    }
+    await writeFile(dest, prettyHeader);
+    console.log("raw header written to", dest);
   }
 }
