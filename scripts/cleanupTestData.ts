f6851325baaebc5a8b76c145e1054252f14c8462
lafkpages <soy.lafk+gh@gmail.com>
2023-10-27 16:41:09 +0200

    Pretest cleanup script rm app dir

--- scripts/cleanupTestData.ts
+++ scripts/cleanupTestData.ts
@@ -6,7 +6,7 @@ import { join as joinPaths } from "path";
 const ignoreDir = "test/ignore";
 
 for (const file of await readdir(ignoreDir)) {
-  if (/\.(js|json|txt|extracted)$|^app-.+\.asar/.test(file)) {
+  if (/^app$|\.(js|json|txt|extracted)$|^app-.+\.asar/.test(file)) {
     await rm(joinPaths(ignoreDir, file), {
       force: true,
       recursive: true,
